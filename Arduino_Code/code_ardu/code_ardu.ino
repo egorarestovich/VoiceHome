0b73741c104a5e52f5932842f45fc7b189552421
Egor Arestovich <arestovitsch@gmail.com>
2025-08-02 17:55:18 +0300

    Update code_ardu.ino
    Refactoring

--- Arduino_Code/code_ardu/code_ardu.ino
+++ Arduino_Code/code_ardu/code_ardu.ino
@@ -1,91 +1,112 @@
 #include <SoftwareSerial.h>
 #include "DHT.h"
 
-#define smokePin A2
+#define SMOKE_SENSOR_PIN   A2
+#define LIGHT_PIN          8
+#define KETTLE_PIN         4
+#define FAN_PIN            2
+#define RELAY_FREE_PIN     5
 
-int sensorThres = 450;
-String ok = "";
-int utqk_ccfr = 0;
+#define DHT_PIN            12
+#define DHT_TYPE           DHT11
+DHT dht(DHT_PIN, DHT_TYPE);
 
-SoftwareSerial mySerialBT(9, 10);
-DHT dht(12, DHT11);
+#define SMOKE_THRESHOLD    450
+
+SoftwareSerial mySerialBT(9, 10);  // RX=9, TX=10
+
+char command = 0;  // Команда от Bluetooth или Serial
 
 void setup() {
+  Serial.begin(9600);
   mySerialBT.begin(9600);
-  Serial.begin(9600);
-  digitalWrite(8, 0);//свет
-  digitalWrite(5, 0);//свободное реле
-  digitalWrite(2, 0);//вентилятор
-  digitalWrite(4, 0);//чайник
+
+  pinMode(LIGHT_PIN,      OUTPUT);
+  pinMode(KETTLE_PIN,     OUTPUT);
+  pinMode(FAN_PIN,        OUTPUT);
+  pinMode(RELAY_FREE_PIN, OUTPUT);
+
+  digitalWrite(LIGHT_PIN,      LOW);
+  digitalWrite(KETTLE_PIN,     LOW);
+  digitalWrite(FAN_PIN,        LOW);
+  digitalWrite(RELAY_FREE_PIN, LOW);
+
   dht.begin();
-  Serial.begin(9600);
+
+  Serial.println("Система запущена.");
 }
 
 void loop() {
-  int analogSensor = analogRead(smokePin);
-  int temp = dht.readTemperature();
-  int vl = dht.readHumidity();
   if (mySerialBT.available()) {
-    utqk_ccfr = mySerialBT.read();
+    command = mySerialBT.read();
   }
   if (Serial.available()) {
-    utqk_ccfr = Serial.read();
+    command = Serial.read();
   }
-  if(utqk_ccfr > 20){
-    utqk_ccfr -= int('0');
+
+  if (command >= '1' && command <= '9') {
+    handleCommand(command - '0');
+    command = 0;
   }
-  if (utqk_ccfr == 1) {
-    pinMode(8, OUTPUT);
-    digitalWrite(8, 1);
+
+  int smokeValue = analogRead(SMOKE_SENSOR_PIN);
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  String status = "Всё хорошо";
+
+  if (smokeValue > SMOKE_THRESHOLD) {
+    status = "В доме обнаружена утечка газа";
+    Serial.println("Внимание: утечка газа!");
   }
-  if (utqk_ccfr == 2) {
-    pinMode(8, OUTPUT);
-    digitalWrite(8, 0);
-  }
-  if (utqk_ccfr == 3) {
-    pinMode(4, OUTPUT);
-    digitalWrite(2, 1);
-  }
-  if (utqk_ccfr == 4) {
-    pinMode(4, OUTPUT);
-    digitalWrite(2, 0);
-  }
-  if (utqk_ccfr == 5) {
-    pinMode(5, OUTPUT);
-    digitalWrite(4, 1);
-  }
-  if (utqk_ccfr == 6) {
-    pinMode(5, OUTPUT);
-    digitalWrite(4, 0);
-  }
-  if (utqk_ccfr == 7) {
-    pinMode(2, OUTPUT);
-    digitalWrite(5, 1);
-  }
-  if (utqk_ccfr == 8) {
-    pinMode(2, OUTPUT);
-    digitalWrite(5, 0);
-  }
-  if (utqk_ccfr == 9) {
-    pinMode(8, OUTPUT);
-       digitalWrite(8, 0);
-      pinMode(5, OUTPUT);
-       digitalWrite(5, 0);
-      pinMode(4, OUTPUT);
-       digitalWrite(4, 0);
-      pinMode(2, OUTPUT);
-       digitalWrite(2, 0);
-  }
-  if (analogSensor > sensorThres) {
-    Serial.println(" Gaz!");
-    ok = "В доме обнаружена утечка газа";
-  }
-  else {
-    ok = "Всё хорошо";
-  }
-  String te = (String(temp) + "°" + ":" + String(vl) + "%" + ":" + ok + ":" + ";");
-  mySerialBT.println(te);
-  Serial.println(te);
-  Serial.println(utqk_ccfr);
+
+  if (isnan(temperature)) temperature = -99;
+  if (isnan(humidity))    humidity    = -99;
+
+  String message = String(temperature) + "°:" +
+                   String(humidity) + "%:" +
+                   status + ":" + ";";
+
+  mySerialBT.println(message);
+  Serial.println(message);
+
   delay(300);
+}
+
+void handleCommand(int cmd) {
+  switch (cmd) {
+    case 1:
+      digitalWrite(LIGHT_PIN, HIGH);
+      break;
+    case 2:
+      digitalWrite(LIGHT_PIN, LOW);
+      break;
+    case 3:
+      digitalWrite(KETTLE_PIN, HIGH);
+      break;
+    case 4:
+      digitalWrite(KETTLE_PIN, LOW);
+      break;
+    case 5:
+      digitalWrite(RELAY_FREE_PIN, HIGH);
+      break;
+    case 6:
+      digitalWrite(RELAY_FREE_PIN, LOW);
+      break;
+    case 7:
+      digitalWrite(FAN_PIN, HIGH);
+      break;
+    case 8:
+      digitalWrite(FAN_PIN, LOW);
+      break;
+    case 9:
+      digitalWrite(LIGHT_PIN,      LOW);
+      digitalWrite(KETTLE_PIN,     LOW);
+      digitalWrite(FAN_PIN,        LOW);
+      digitalWrite(RELAY_FREE_PIN, LOW);
+      break;
+    default:
+      Serial.print("Неизвестная команда: ");
+      Serial.println(cmd);
+      break;
+  }
 }
